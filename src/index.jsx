ec2954288bdd8b704b4f41e467ea76632a43ca50
Slava <anitalorack@ya.ru>
2020-01-18 05:23:25 +0500

    New Commit

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom'
 // eslint-disable-next-line import/no-unresolved
 import App from 'Components/App'
 import { I18nProvider } from '@lingui/react'
-import catalogEn from 'locales/en/messages.json'
+import catalogEn from './locales/en/messages.json'
+import catalogRu from './locales/ru/messages.json'
+import catalogFr from './locales/fr/messages.json'
+import catalogEs from './locales/es/messages.json'
 
-const catalogs = { en: catalogEn }
+const catalogs = {
+  en: catalogEn,
+  ru: catalogRu,
+  fr: catalogFr,
+  es: catalogEs,
+}
 
 const Index = () => {
   return (
